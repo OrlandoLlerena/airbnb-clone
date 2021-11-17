8e779f8daeaf7b569f2bf92c3d4e7b3d403b7927
orlandollerena <trancetribal@bellsouth.net>
2021-11-17 15:29:32 -0500

    add number of guest section of calendar

--- components/Header.js
+++ components/Header.js
@@ -15,11 +15,12 @@ function Header() {
   const [searchInput, setSearchInput] = useState();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [numOfGuests, setNumOfGuests] = useState(1);
 
   const selectionRange = {
     startDate: startDate,
     endDate: endDate,
-    key: "Selection",
+    key: "selection",
   };
 
   const handleSelect = (ranges) => {
@@ -63,13 +64,26 @@ function Header() {
       </div>
 
       {searchInput && (
-        <div>
+        <div className="flex flex-col col-span-3 mx-auto">
           <DateRangePicker
             ranges={[selectionRange]}
             minDate={new Date()}
             rangeColors={["#FD5B61"]}
             onChange={handleSelect}
           />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={numOfGuests}
+              onChange={(e) => setNumOfGuests(e.target.value)}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+              type="number"
+              min={1}
+            />
+          </div>
         </div>
       )}
     </header>
